e1e89c5ef8c1c03df6b8c3cb00b2909a67eed331
David Blincoe <dblincoe@gmail.com>
2019-10-05 18:38:04 -0400

    Intial commit for java

--- p2p.java
+++ p2p.java
@@ -2,6 +2,6 @@ public class p2p
 {
     public static void main(String[] args)
     {
-    
+        System.out.println("Hello World");
     }
 }
--- p2p.sh
+++ p2p.sh
@@ -12,38 +12,39 @@ run_remote_commands() {
 
 	for server in $SERVER_LIST
 	do
-		echo -e "\n Connecting to ${server}"
+		echo -e "\nConnecting to ${server}"
 		ssh $server "cd ./P2P-Network; ./p2p.sh ${1}" > /dev/null 2>&1
-		echo -e "Finished Running Commands on ${server} \n"
+		echo "Finished Running Commands on ${server}"
 	done
 }
 
 if [ "$1" == "remote_pull" ]
 then
 	run_remote_commands "local_pull"
+
 elif [ "$1" == "local_pull" ]
 then
 	git pull origin master > out.log
+
 elif [ "$1" == "help" ] || [ -z "$1" ]
 then
-	echo -e '\n'
-	echo -e 'To see this help page again the help command can be used!'
-	echo -e '\n'
+	echo -e ''
+	echo -e 'General Commands: '
+	echo -e '\t help: load the help page'
+	echo -e ''
 
 	echo 'Remote Commands: '
 	echo -e '\t remote_pull: pulls the latest code remotely'
 	echo -e '\t remote_compile: compiles code remotely'
-	echo ''
 	echo -e '\t remote_start: starts p2p network remotely'
 	echo -e '\t remote_stop: stops p2p network remotely'
-	echo -e '\n'
+	echo -e ''
 
 	echo 'Local Commands: '
 	echo -e '\t local_pull: pulls the latest code on a local host'
 	echo -e '\t local_compile: compiles code on a local host'
-	echo ''
 	echo -e '\t local_start: starts p2p network locally'
 	echo -e '\t local_stop: stops p2p network locally'
 
-	echo -e '\n'
+	echo -e ''
 fi
