5b90a971a2bbcddd18539743e009f42dc3963a1b
David Blincoe <dblincoe@gmail.com>
2019-10-23 21:32:09 -0400

    Screw git. Using scp

--- p2p.sh
+++ p2p.sh
@@ -1,39 +1,23 @@
 #!/bin/bash
 
 SERVER_LIST=`cat ./servers.txt`
-run_remote_commands() {
-	if [ -z "$1" ]
-	then
-		return
-	fi
-
+if [ "$1" == "remote_pull" ]
+then
 	for server in $SERVER_LIST
 	do
 		echo -e "\nConnecting to ${server}"
-		ssh $server "cd ./p2p; ./p2p.sh ${1}" > /dev/null 2>&1
-		echo "Finished Running Commands on ${server}"
+		scp -r ./src/ $server:/home/drb133/p2p/src
+		echo "Finished copying sources to ${server}"
 	done
-}
-
-if [ "$1" == "remote_pull" ]
-then
-	echo "Pulling git repo..."
-	run_remote_commands "local_pull"
 
 elif [ "$1" == "remote_compile" ]
 then
-	echo "Compiling code..."
-	run_remote_commands "local_compile"
-
-elif [ "$1" == "local_pull" ]
-then
-	git fetch --all
-        git reset --hard origin/master
-
-elif [ "$1" == "local_compile" ]
-then
-	javac -d ./ ./src/*.java
-
+	for server in $SERVER_LIST
+	do
+		echo -e "\nConnecting to ${server}"
+		ssh $server "cd ./p2p; javac -d ./ ./src/*.java" > /dev/null 2>&1
+		echo "Finished Compiling on ${server}"
+	done
 elif [ "$1" == "help" ] || [ -z "$1" ]
 then
 	echo -e ''
@@ -44,15 +28,5 @@ then
 	echo 'Remote Commands: '
 	echo -e '\t remote_pull: pulls the latest code remotely'
 	echo -e '\t remote_compile: compiles code remotely'
-	echo -e '\t remote_start: starts p2p network remotely'
-	echo -e '\t remote_stop: stops p2p network remotely'
-	echo -e ''
-
-	echo 'Local Commands: '
-	echo -e '\t local_pull: pulls the latest code on a local host'
-	echo -e '\t local_compile: compiles code on a local host'
-	echo -e '\t local_start: starts p2p network locally'
-	echo -e '\t local_stop: stops p2p network locally'
-
 	echo -e ''
 fi
